220c1eee67e86e9023b71949c1b9427f201b2492
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-10-25 21:19:40 +0100

    feat(filestorage): add delete method, all filter

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,17 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        filtred_objs = {}
+
+        for key in FileStorage.__objects:
+            obj = FileStorage.__objects[key]
+            if obj.__class__.__name__ == cls.__name__:
+                filtred_objs[key] = obj
+        return filtred_objs
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +32,13 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """Delete obj"""
+        if obj is None:
+            return
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects.pop(key)
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -45,6 +60,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
