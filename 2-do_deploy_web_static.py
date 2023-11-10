9c11a85437cb997b3736f184ebd23be692277063
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-11-09 17:27:20 -0800

    fix: move web_static content to release folder

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,6 +6,7 @@ from fabric.api import put, run, env
 from pathlib import Path
 
 env.hosts = ['100.24.237.103', '54.175.189.210']
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
@@ -13,13 +14,14 @@ def do_deploy(archive_path):
     deploy web_static archive
     """
 
-    archive_basename = Path(archive_path).stem
-    release_path = "/data/web_static/releases/{}".format(archive_basename)
+    basename = Path(archive_path).stem
+    release_path = "/data/web_static/releases/{}".format(basename)
 
     try:
         put(archive_path, "/tmp")
         run("mkdir -p {}".format(release_path))
-        run("tar -xfz /tmp/{} -C {}".format(archive_path, release_path))
+        run("tar -xzf /tmp/{}.tgz -C {}".format(basename, release_path))
+        run("mv {}/web_static/* {}/".format(release_path, release_path))
         run("rm -f /tmp/{}".format(archive_path))
         run("rm -f /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(release_path))
