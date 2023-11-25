679321cd7324e92c4c341b88b9c9a7a029a75d5c
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-11-26 00:07:48 +0100

    fix: base model schema

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ Base = orm.declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
 
-    id = Column(String(60), primary_key=True, nullable=False, unique=True)
+    id = Column(String(60), primary_key=True, default=uuid.uuid4)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
